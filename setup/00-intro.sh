bbbf58192f5400778afe85124bb7c146e63d6de1
Jan Weitz <jan.weitz@dkb.de>
2024-03-20 21:43:26 +0100

    initial tutorial

--- setup/00-intro.sh
+++ setup/00-intro.sh
@@ -42,21 +42,21 @@ rm -f .env
 # Control Plane Cluster #
 #########################
 
-kind create cluster --config kind.yaml
+systemd-run --scope kind create cluster --config kind.yaml
 
 kubectl apply \
-    --filename https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+	--filename https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 
 ##############
 # Crossplane #
 ##############
 
 helm upgrade --install crossplane crossplane \
-    --repo https://charts.crossplane.io/stable \
-    --namespace crossplane-system --create-namespace --wait
+	--repo https://charts.crossplane.io/stable \
+	--namespace crossplane-system --create-namespace --wait
 
 kubectl apply \
-    --filename providers/provider-kubernetes-incluster.yaml
+	--filename providers/provider-kubernetes-incluster.yaml
 
 kubectl apply --filename providers/provider-helm-incluster.yaml
 
@@ -67,171 +67,46 @@ kubectl apply --filename providers/dot-sql.yaml
 kubectl apply --filename providers/dot-app.yaml
 
 gum spin --spinner dot \
-    --title "Waiting for Crossplane providers..." -- sleep 60
+	--title "Waiting for Crossplane providers..." -- sleep 60
 
 kubectl wait --for=condition=healthy provider.pkg.crossplane.io \
-    --all --timeout=1800s
+	--all --timeout=1800s
 
-echo "## Which Hyperscaler do you want to use?" | gum format
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: aws-creds
+  namespace: crossplane-system
+stringData:
+  creds: |
+    [default]
+    aws_access_key_id = test
+    aws_secret_access_key = test
+EOF
 
-HYPERSCALER=$(gum choose "google" "aws" "azure")
-
-echo "export HYPERSCALER=$HYPERSCALER" >> .env
-
-if [[ "$HYPERSCALER" == "google" ]]; then
-
-    gcloud auth login
-
-    PROJECT_ID=dot-$(date +%Y%m%d%H%M%S)
-
-    echo "export PROJECT_ID=$PROJECT_ID" >> .env
-
-    gcloud projects create ${PROJECT_ID}
-
-    open "https://console.cloud.google.com/billing/linkedaccount?project=$PROJECT_ID"
-
-    echo "## LINK A BILLING ACCOUNT" | gum format
-    gum input --placeholder "Press the enter key to continue."
-
-    open "https://console.cloud.google.com/marketplace/product/google/container.googleapis.com?project=$PROJECT_ID"
-
-    echo "## *ENABLE* the API" | gum format
-    gum input --placeholder "Press the enter key to continue."
-
-    open "https://console.cloud.google.com/apis/library/sqladmin.googleapis.com?project=$PROJECT_ID"
-
-    echo "## *ENABLE* the API" | gum format
-    gum input --placeholder "Press the enter key to continue."
-
-    export SA_NAME=devops-toolkit
-
-    export SA="${SA_NAME}@${PROJECT_ID}.iam.gserviceaccount.com"
-
-    gcloud iam service-accounts create $SA_NAME \
-        --project $PROJECT_ID
-
-    export ROLE=roles/admin
-
-    gcloud projects add-iam-policy-binding \
-        --role $ROLE $PROJECT_ID --member serviceAccount:$SA
-
-    gcloud iam service-accounts keys create gcp-creds.json \
-        --project $PROJECT_ID --iam-account $SA
-
-    kubectl --namespace crossplane-system \
-        create secret generic gcp-creds \
-        --from-file creds=./gcp-creds.json
-
-    echo "
-apiVersion: gcp.upbound.io/v1beta1
+cat <<EOF | kubectl apply -f -
+apiVersion: aws.upbound.io/v1beta1
 kind: ProviderConfig
 metadata:
   name: default
 spec:
-  projectID: $PROJECT_ID
   credentials:
     source: Secret
     secretRef:
+      name: aws-creds
       namespace: crossplane-system
-      name: gcp-creds
-      key: creds" | kubectl apply --filename -
-
-elif [[ "$HYPERSCALER" == "aws" ]]; then
-
-    AWS_ACCESS_KEY_ID=$(gum input --placeholder "AWS Access Key ID" --value "$AWS_ACCESS_KEY_ID")
-    echo "export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" >> .env
-    
-    AWS_SECRET_ACCESS_KEY=$(gum input --placeholder "AWS Secret Access Key" --value "$AWS_SECRET_ACCESS_KEY" --password)
-    echo "export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" >> .env
-
-    AWS_ACCOUNT_ID=$(gum input --placeholder "AWS Account ID" --value "$AWS_ACCOUNT_ID")
-    echo "export AWS_ACCOUNT_ID=$AWS_ACCOUNT_ID" >> .env
-
-    echo "[default]
-aws_access_key_id = $AWS_ACCESS_KEY_ID
-aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
-" >aws-creds.conf
-
-    kubectl --namespace crossplane-system \
-        create secret generic aws-creds \
-        --from-file creds=./aws-creds.conf
-
-    kubectl apply --filename providers/aws-config.yaml
-
-else
-
-    AZURE_TENANT_ID=$(gum input --placeholder "Azure Tenant ID" --value "$AZURE_TENANT_ID")
-
-    az login --tenant $AZURE_TENANT_ID
-
-    export SUBSCRIPTION_ID=$(az account show --query id -o tsv)
-
-    az ad sp create-for-rbac --sdk-auth --role Owner --scopes /subscriptions/$SUBSCRIPTION_ID | tee azure-creds.json
-
-    kubectl --namespace crossplane-system create secret generic azure-creds --from-file creds=./azure-creds.json
-
-    kubectl apply --filename providers/azure-config.yaml
-
-    DB_NAME=silly-demo-db-$(date +%Y%m%d%H%M%S)
-
-    echo "---
-apiVersion: devopstoolkitseries.com/v1alpha1
-kind: ClusterClaim
-metadata:
-  name: cluster-01
-spec:
-  id: cluster01
-  compositionSelector:
-    matchLabels:
-      provider: azure
-      cluster: aks
-  parameters:
-    nodeSize: small
-    minNodeCount: 3
----
-apiVersion: v1
-kind: Secret
-metadata:
-  name: $DB_NAME-password
-data:
-  password: SVdpbGxOZXZlclRlbGxAMQ==
----
-apiVersion: devopstoolkitseries.com/v1alpha1
-kind: SQLClaim
-metadata:
-  name: silly-demo-db
-spec:
-  id: $DB_NAME
-  compositionSelector:
-    matchLabels:
-      provider: azure
-      db: postgresql
-  parameters:
-    version: \"11\"
-    size: small
----
-apiVersion: devopstoolkitseries.com/v1alpha1
-kind: AppClaim
-metadata:
-  name: silly-demo
-spec:
-  id: silly-demo
-  compositionSelector:
-    matchLabels:
-      type: backend-db
-      location: remote
-  parameters:
-    namespace: production
-    image: c8n.io/vfarcic/silly-demo:1.4.52
-    port: 8080
-    host: silly-demo.acme.com
-    dbSecret:
-      name: silly-demo-db
-      namespace: a-team
-    kubernetesProviderConfigName: cluster01" \
-    | tee examples/azure-intro.yaml
-
-fi
+      key: creds
+  endpoint:
+    hostnameImmutable: true
+    url:
+      type: Static
+      static: http://host.docker.internal:4566
+  skip_credentials_validation: true
+  skip_metadata_api_check: true
+  skip_requesting_account_id: true
+  s3_use_path_style: true
+EOF
 
 kubectl create namespace a-team
 
@@ -241,11 +116,11 @@ kubectl create namespace a-team
 
 REPO_URL=$(git config --get remote.origin.url)
 
-yq --inplace ".spec.source.repoURL = \"$REPO_URL\"" argocd/apps.yaml
+# yq --in-place ".spec.source.repoURL = \"$REPO_URL\"" argocd/apps.yaml
 
 helm upgrade --install argocd argo-cd \
-    --repo https://argoproj.github.io/argo-helm \
-    --namespace argocd --create-namespace \
-    --values argocd/helm-values.yaml --wait
+	--repo https://argoproj.github.io/argo-helm \
+	--namespace argocd --create-namespace \
+	--values argocd/helm-values.yaml --wait
 
 kubectl apply --filename argocd/apps.yaml
